b31630b4e8cbdc1caa37b078264571dc0320d83e
Vova Merkulov <vova.merkulov.09@mail.ru>
2023-09-28 21:15:26 +0700

    'fix'

--- v3.2.py
+++ v3.2.py
@@ -2,9 +2,9 @@ f = int(input('f: '))
 c = int(input('c: '))
 
 if f % c == 0:
-        print('деление не возможно')
+        print('деление возможно')
         print(f / c) 
 else:
-        print('деление  возможно')
-        print(f / c)                                                                                                                                                                     
+        print('деление не возможно')
+        print(f / c) 
      
